1c495b6f101cfb8e59dc9945f48c40c5bea1c419
Haeun Kim <haeun159951@gmail.com>
2020-07-01 16:32:23 -0400

    Only popup files
    initial commit

---
+++ Only PoP Up /js/main.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+    $(".btn").on('click', function(){
+  $(".cover").fadeIn('slow');    
+  $(".popup").fadeIn('slow');
+    });
+    $(".popup").on('click', function(){
+      if($(event.target).is("#close")){
+          $(".cover").fadeOut('slow');    $(".popup").fadeOut('slow'); 
+      }
+    });
+    
+    $('.cover').on('click', function(){
+       $(".cover").fadeOut('slow');    $(".popup").fadeOut('slow'); 
+    });
+
+
+    $('#picker').datetimepicker({
+      timepicker: true,
+      datepicker: true,
+      format: 'Y-m-d H:i', // formatDate
+      value: '2020-07-03',
+      hours12: false,
+      step: 1
+  })
+  });
+
+
+ 
